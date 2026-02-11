1bbee5d23b4537a99edf260815c8fc9090a3523c
Leonard Atorough <leotorough@gmail.com>
2026-02-11 21:54:58 +0000

    refactor: Remove link property from projectsCollection schema in config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        imageUrl: z.string(),
-        link: z.string(),
-        tags: z.array(z.string()).optional(),
-        liveUrl: z.string().optional(),
-        githubUrl: z.string().optional(),
-    }),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    imageUrl: z.string(),
+    tags: z.array(z.string()).optional(),
+    liveUrl: z.string().optional(),
+    githubUrl: z.string().optional(),
+  }),
 });
 
 export const collections = {
-    projects: projectsCollection,
+  projects: projectsCollection,
 };
